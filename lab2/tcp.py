b63ae396dbf4f61e752d4d64295cf4d823048994
Tyred <yuri.gabrielaragaobol@gmail.com>
2021-05-24 02:09:37 -0300

    Passos 1 e 2 feitos

--- lab2/tcp.py
+++ lab2/tcp.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+import struct
+import sys
 from tcputils import *
 
 
@@ -34,7 +37,19 @@ class Servidor:
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+
+            # Certificando que o seq do segundo aperto nao e 0.
+            seq_noHandShake = 0
+            while seq_noHandShake == 0: # ARRUMAR ISSO AQUI DEPOIS ...
+                seq_noHandShake = struct.unpack('I', os.urandom(4))[0] % 60500
+
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no+1, ack_no, seq_noHandShake+1, src_addr, src_port, dst_addr, dst_port) 
+            
+            # Segundo aperto da mao.
+            segmentDest = fix_checksum(make_header(dst_port , src_port, seq_noHandShake, (seq_no+1), FLAGS_SYN | FLAGS_ACK), dst_addr, src_addr)
+            
+            self.rede.enviar(segmentDest, src_addr)
+            # Envia um pacote com SYN, ACK (ackNum = seqNum+1)
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
@@ -48,9 +63,18 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, expectedSeqNum, ackNum,seq_noHandShake, srcAddr, srcPort, dstAddr, dstPort):
         self.servidor = servidor
         self.id_conexao = id_conexao
+        # Adicionado: seq e ack e enderecos e portas.
+        self.expectedSeqNum = expectedSeqNum # Usado no passo 2
+        self.ackNum = ackNum # Usado no passo 3
+        self.seq_noHandShake = seq_noHandShake # Usado no passo 3
+        self.srcAddr = srcAddr
+        self.srcPort = srcPort
+        self.dstAddr = dstAddr
+        self.dstPort = dstPort
+        # ---------------------
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
@@ -63,6 +87,15 @@ class Conexao:
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
+
+        # Apenas necessario garantir de ACK o pacote correto enviado.
+        if seq_no == self.expectedSeqNum:
+            segmentDest = fix_checksum(make_header(self.dstPort , self.srcPort, seq_no, (seq_no+len(payload)), FLAGS_ACK), self.dstAddr, self.srcAddr) # Montando pacote de ACK
+            self.seq_no = seq_no
+            self.servidor.rede.enviar(segmentDest, self.srcAddr) # Enviando o pacote ACK montado
+            self.expectedSeqNum = self.expectedSeqNum + len(payload) # Atulizando o proximo seq esperado.
+            self.callback(self, payload) # Mandando para a camada de aplicação. (TIPO: HTTTP ou o NC)
+
         print('recebido payload: %r' % payload)
 
     # Os métodos abaixo fazem parte da API
@@ -74,14 +107,37 @@ class Conexao:
         """
         self.callback = callback
 
-    def enviar(self, dados):
+    def enviar(self, dados): # Passo 3.
         """
         Usado pela camada de aplicação para enviar dados
         """
         # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
-        pass
+        # Lembrar de dividir o payload em MSG...
+        ## --- Ta enviando mas esta enviando errado ---
+        ## Como enfiar o payload nessa bagaça
+        #print(self.ackNum)
+        #print(self.expectedSeqNum)
+        #print(self.seq_no)
+
+        # ------------------------------------------------------------------------------------
+        # TODO: EXPECTEDSeqNum se mantem igual, o que vai incrementar é o seq_noHandShake.
+        # - Incrementar eles com o tamanho do payload do segmento.
+        # - Descobrir como inserir um payload no segmento.
+        # - Dividir os pacotes que serao inviados em MSG.
+        # - Lembrar de incrementar a variavel na medidade do tamanho dos pacotes.
+
+        # Dividindo dados em partes com tamanho MSS.
+        ## lista de partes MSS de dados.
+        partes = [dados[i:i+MSS] for i in range(0, len(dados), MSS)]
+
+        ## POR ALGUM MOTIVO NAO TA CHEGANDO O PAYLOAD ...
+        for parte in partes:
+            segmentDest = fix_checksum(make_header(self.srcPort , self.dstPort, self.seq_noHandShake, self.expectedSeqNum, FLAGS_ACK) + parte, self.srcAddr, self.dstAddr) 
+            self.servidor.rede.enviar(segmentDest, self.dstAddr) 
+            self.expectedSeqNum = self.expectedSeqNum + len(parte)
+            #self.callback(self, parte)
 
     def fechar(self):
         """
